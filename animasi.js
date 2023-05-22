5ce303d91afb188fdbfc2b16d4311990848925c1
Yorisfad <117644112+Yorisfad@users.noreply.github.com>
2023-05-22 15:16:59 +0700

    Delete animasi.js

--- animasi.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var elements = document.querySelectorAll('.fade-in');
-  
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.animationDelay = i * 200 + 'ms';
-  }
-});
